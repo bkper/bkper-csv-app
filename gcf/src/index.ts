a909948b34fb80d53392238ddc96e443d507767a
Bruno Coelho <bbcoelho@gmail.com>
2024-09-13 18:59:28 -0300

    chore: index entry point

--- gcf/src/index.ts
+++ gcf/src/index.ts
@@ -1,5 +1,71 @@
-import { http } from '@google-cloud/functions-framework';
+import { HttpFunction } from '@google-cloud/functions-framework/build/src/functions';
+import { Bkper } from 'bkper-js';
+import { Request, Response } from 'express';
+import 'source-map-support/register.js';
+import express from 'express';
+import httpContext from 'express-http-context';
 
-http('doPost', (req, res) => {
-  res.status(200).send('HTTP with Node.js in GCF 2nd gen!');
-});
+// import { EventHandlerTransactionPosted } from './EventHandlerTransactionPosted.js';
+
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
+dotenv.config({ path: `${__dirname}/../../.env` });
+
+const app = express();
+app.use(httpContext.middleware);
+app.use('/', handleEvent);
+export const doPost: HttpFunction = app;
+
+export type Result = {
+    result?: string[] | string | boolean,
+    error?: string,
+    warning?: string
+}
+
+function init(req: Request, res: Response) {
+    res.setHeader('Content-Type', 'application/json');
+
+    //Put OAuth token from header in the http context for later use when calling the API. https://julio.li/b/2016/10/29/request-persistence-express/
+    const oauthTokenHeader = 'bkper-oauth-token';
+    httpContext.set(oauthTokenHeader, req.headers[oauthTokenHeader]);
+
+    Bkper.setConfig({
+        oauthTokenProvider: async () => httpContext.get(oauthTokenHeader) || import('bkper').then(bkper => bkper.getOAuthToken()),
+        apiKeyProvider: async () => process.env.BKPER_API_KEY || req.headers['bkper-api-key'] as string
+    })
+}
+
+async function handleEvent(req: Request, res: Response) {
+
+    init(req, res);
+
+    try {
+
+        let event: bkper.Event = req.body
+        let result: Result = { result: false };
+
+        switch (event.type) {
+            case 'FILE_CREATED':
+                // result = await new EventHandlerTransactionPosted().handleEvent(event);
+                result = { result: "TESTE" };
+                break;
+        }
+
+        res.send(response(result));
+
+    } catch (err: any) {
+        console.error(err);
+        res.send(response({ error: err.stack ? err.stack.split("\n") : err }));
+    }
+
+}
+
+function response(result: Result): string {
+    const body = JSON.stringify(result, null, 4);
+    return body;
+}
