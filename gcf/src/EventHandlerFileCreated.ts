8eef5dcc90f5a79ded142c5ad76b6ea7696d98f9
victordrumond <victordrumond@hotmail.com>
2025-11-10 15:56:51 -0300

    Comment out EventHandlerFileCreated handler

--- gcf/src/EventHandlerFileCreated.ts
+++ gcf/src/EventHandlerFileCreated.ts
@@ -4,32 +4,32 @@ import { Result } from "./index.js";
 export class EventHandlerFileCreated {
 
     async handleEvent(event: bkper.Event): Promise<Result> {
-        if (event.bookId && event.data) {
-            const book = await Bkper.getBook(event.bookId);
-            if (event.data.object) {
-                const fileEventObj: bkper.File = event.data.object;
-                if (fileEventObj.id) {
-                    const file = await book.getFile(fileEventObj.id);
-                    const fileContentBase64 = await file.getContent();
-                    if (fileContentBase64) {
-                        const fileContentUtf8 = Buffer.from(fileContentBase64, 'base64').toString('utf-8');
-                        if (fileContentUtf8) {
-                            const lines = fileContentUtf8.split('\n')
-                            let transactions: Transaction[] = [];
-                            for (let i = 1; i < lines.length; i++) {
-                                const line = lines[i];
-                                const description = line.split(';').map(e => e.split(',')).join(' ');
-                                const transaction = new Transaction(book).setDescription(description);
-                                transactions.push(transaction);
-                            }
-                            await book.batchCreateTransactions(transactions);
+        // if (event.bookId && event.data) {
+        //     const book = await Bkper.getBook(event.bookId);
+        //     if (event.data.object) {
+        //         const fileEventObj: bkper.File = event.data.object;
+        //         if (fileEventObj.id) {
+        //             const file = await book.getFile(fileEventObj.id);
+        //             const fileContentBase64 = await file.getContent();
+        //             if (fileContentBase64) {
+        //                 const fileContentUtf8 = Buffer.from(fileContentBase64, 'base64').toString('utf-8');
+        //                 if (fileContentUtf8) {
+        //                     const lines = fileContentUtf8.split('\n')
+        //                     let transactions: Transaction[] = [];
+        //                     for (let i = 1; i < lines.length; i++) {
+        //                         const line = lines[i];
+        //                         const description = line.split(';').map(e => e.split(',')).join(' ');
+        //                         const transaction = new Transaction(book).setDescription(description);
+        //                         transactions.push(transaction);
+        //                     }
+        //                     await book.batchCreateTransactions(transactions);
 
-                            return { result: `Recorded ${transactions.length} transaction(s).` }
-                        }
-                    }
-                }
-            }
-        }
+        //                     return { result: `Recorded ${transactions.length} transaction(s).` }
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
         return { result: false };
     }
 
